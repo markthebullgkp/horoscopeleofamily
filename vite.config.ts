b926d828ba4c5fe865939ea9ece8403f9ded5751
markthebullgkp <120301477+markthebullgkp@users.noreply.github.com>
2026-01-08 19:41:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,25 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Environment variables load करने के लिए
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    // Files bahar hain isliye base '/' rakhein
     base: '/', 
     plugins: [react()],
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    preview: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'), // अक्सर '@' को 'src' के लिए यूज किया जाता है
+        // '@' ab seedhe root folder ko point karega
+        '@': path.resolve(__dirname, './'),
       }
     }
   };
